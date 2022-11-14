91894d44fcd391432917593c8b8bc7a1625864af
Maksym Kochut <feelofgoodvibes@gmail.com>
2022-11-14 10:13:55 +0200

    Add models

--- models.py
+++ models.py
@@ -4,3 +4,24 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 from datetime import datetime
+
+
+class ShortURL(Base):
+    __tablename__ = "shorturl"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String(2083))
+    shortcode = Column(String, unique=True)
+
+    stats = relationship("Statistics", backref="shorturl")
+
+
+class Statistics(Base):
+    __tablename__ = "statistics"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    short_url = Column(Integer, ForeignKey("shorturl.id"))
+    visit_time = Column(DateTime)
+
+
+
